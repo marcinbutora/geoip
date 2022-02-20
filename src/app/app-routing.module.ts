1a87a8ce947a690fb73b928759e2251bcc3ecac3
mb83pl <Doctype1983!>
2022-02-20 14:47:31 +0100

    home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {IpInfoComponent} from "./pages/ip/ip-info/ip-info.component";
+import {HomeComponent} from "./layout/home/home.component";
 
 const routes: Routes = [
-  {path: 'ip/:ip', component: IpInfoComponent}
+  {path: 'ip/:ip', component: IpInfoComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 import {IpInfoComponent} from "./pages/ip/ip-info/ip-info.component";
 import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './layout/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IpInfoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
