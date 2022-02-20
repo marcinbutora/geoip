66724d508a9ddaf3bcd458a020ed2ef4c3c1301b
mb83pl <Doctype1983!>
2022-02-20 19:13:50 +0100

    service new layout

--- src/app/pages/ip/ip-data.service.ts
+++ src/app/pages/ip/ip-data.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {IP} from "./ip-model/ip";
 import * as mapboxgl from "mapbox-gl";
+import {Map, Marker, Popup} from "mapbox-gl";
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,35 @@ export class IpDataService {
   map!: mapboxgl.Map;
   marker!: mapboxgl.Marker;
   popup!: mapboxgl.Popup;
+  apiUrl: string = 'https://ipapi.co/';
 
   constructor(private http: HttpClient) {
     (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWI4M3BsIiwiYSI6ImNrenU2dmZqYzFhbmgybm9odWR3MW1zbGEifQ.FkK9NeB26U89mDa1q_DQkQ';
   }
 
   getDataByIP = (ip: string): Observable<IP> => {
-    return this.http.get<IP>(`https://ipapi.co/${ip}/json/`)
+    return this.http.get<IP>(`${this.apiUrl}${ip}/json/`)
+  }
+
+  addMarkerToMap(data: IP, map: mapboxgl.Map) {
+    this.marker = new Marker()
+      .setLngLat([data.longitude, data.latitude])
+      .addTo(map)
+  }
+
+  addPopupToMarker(data: IP, map: mapboxgl.Map) {
+    this.popup = new Popup()
+      .setLngLat([data.longitude, data.latitude])
+      .addTo(map)
+  }
+
+  displayMap(data: IP) {
+    return new Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 12,
+      center: [data.longitude, data.latitude]
+    });
   }
 
 
--- src/app/pages/ip/ip-info/ip-info.component.ts
+++ src/app/pages/ip/ip-info/ip-info.component.ts
@@ -12,9 +12,7 @@ import {Map, Marker, Popup} from 'mapbox-gl';
 })
 export class IpInfoComponent implements OnInit {
   @Input() data: IP | undefined;
-  @Input() ip: string = '';
-  lat!: number;
-  flagReplace: string = '';
+  ip: string = '';
 
   constructor(private ipService: IpDataService, private route: ActivatedRoute) {
   }
@@ -24,32 +22,10 @@ export class IpInfoComponent implements OnInit {
     this.ipService.getDataByIP(this.ip).subscribe((data) => {
       this.data = data;
 
-      const map = this.displayMap(data);
-      this.addMarkerToMap(data, map);
-      this.addPopupToMarker(data, map);
-      this.flagReplace = data.country_tld.replace(".", "");
+      const map = this.ipService.displayMap(data);
+      this.ipService.addMarkerToMap(data, map);
+      this.ipService.addPopupToMarker(data, map);
     })
-  }
-
-  private addMarkerToMap(data: IP, map: mapboxgl.Map) {
-    this.ipService.marker = new Marker()
-      .setLngLat([data.longitude, data.latitude])
-      .addTo(map)
-  }
-
-  private addPopupToMarker(data: IP, map: mapboxgl.Map) {
-    this.ipService.popup = new Popup()
-      .setLngLat([data.longitude, data.latitude])
-      .addTo(map)
-  }
-
-  private displayMap(data: IP) {
-    return new Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 12,
-      center: [data.longitude, data.latitude]
-    });
   }
 }
 
