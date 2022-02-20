3dad5f584f605d7ed3c67278484f8bf1d03e1997
mb83pl <Doctype1983!>
2022-02-20 19:58:19 +0100

    Get own IP and display map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {IpInfoComponent} from "./pages/ip/ip-info/ip-info.component";
 import {HomeComponent} from "./layout/home/home.component";
+import {IpClientComponent} from "./pages/ip/ip-client/ip-client.component";
 
 const routes: Routes = [
   {path: 'ip/:ip', component: IpInfoComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'ip', component: IpClientComponent},
+  {path: '', pathMatch: 'full', redirectTo: '/ip'},
+  {path: '**', redirectTo: '/ip'}
 ];
 
 @NgModule({
--- src/app/pages/ip/ip-client/ip-client.component.ts
+++ src/app/pages/ip/ip-client/ip-client.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IpClient} from "../ip-model/ipclient";
 import {IpDataService} from "../ip-data.service";
+import {IP} from "../ip-model/ip";
 
 @Component({
   selector: 'app-ip-client',
@@ -9,13 +10,22 @@ import {IpDataService} from "../ip-data.service";
 })
 export class IpClientComponent implements OnInit {
   @Input() data!: IpClient;
+  @Input() data2!: IP;
+  ip!: string;
 
   constructor(private ipService: IpDataService) { }
 
   ngOnInit(): void {
-    this.ipService.getClientIP().subscribe((data) => {
-      this.data = data;
-    })
+    this.ipService.getClientIP().subscribe((dat) => {
+      this.data = dat;
+      this.ipService.getDataByIP(dat.ip).subscribe((d) => {
+        this.data2 = d;
+        const map = this.ipService.displayMap(d);
+        this.ipService.addMarkerToMap(d, map);
+        this.ipService.addPopupToMarker(d, map);
+        console.log(d.country_tld)
+      })
+      })
   }
 
 }
