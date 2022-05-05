6fcc826d7cc47c8e7f4e97698710ffad0db1bd3a
mb83pl <mbutora@gmail.com>
2022-05-05 19:50:42 +0200

    new update info

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   author: string = 'Marcin Butora';
-  dateInfo: Date = new Date("2022-02-27 13:27:00");
-
+  dateInfo: Date = new Date('2022-05-05 19:47');
 }
