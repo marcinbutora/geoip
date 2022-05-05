8ee362209ece41d28f3036bf1f9ff45bf4657e83
mb83pl <mbutora@gmail.com>
2022-05-05 19:41:20 +0200

    changes

--- src/app/pages/ip/ip-data.service.ts
+++ src/app/pages/ip/ip-data.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {IP} from "./ip-model/ip";
-import * as mapboxgl from "mapbox-gl";
-import {Map, Marker} from "mapbox-gl";
-import {IpClient} from "./ip-model/ipclient";
-import {IpWeather} from "./ip-model/ipweather";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IP } from './ip-model/ip';
+import * as mapboxgl from 'mapbox-gl';
+import { Map, Marker } from 'mapbox-gl';
+import { IpClient } from './ip-model/ipclient';
+import { IpWeather } from './ip-model/ipweather';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IpDataService {
-
   map!: mapboxgl.Map;
   marker!: mapboxgl.Marker;
   popup!: mapboxgl.Popup;
-
 
   apiUrl: string = 'https://ipapi.co/';
   apiClient: string = 'https://api.ipify.org/?format=json';
@@ -23,25 +21,28 @@ export class IpDataService {
   apiWeatherKey: string = '8d1da24a8faf953ff79f47841b4e5fef';
 
   constructor(private http: HttpClient) {
-    (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWI4M3BsIiwiYSI6ImNrenU2dmZqYzFhbmgybm9odWR3MW1zbGEifQ.FkK9NeB26U89mDa1q_DQkQ';
+    (mapboxgl as any).accessToken =
+      'pk.eyJ1IjoibWI4M3BsIiwiYSI6ImNrenU2dmZqYzFhbmgybm9odWR3MW1zbGEifQ.FkK9NeB26U89mDa1q_DQkQ';
   }
 
   getDataByIP = (ip: string): Observable<IP> => {
-    return this.http.get<IP>(`${this.apiUrl}${ip}/json/`)
-  }
+    return this.http.get<IP>(`${this.apiUrl}${ip}/json/`);
+  };
 
   getClientIP = (): Observable<IpClient> => {
-    return this.http.get<IpClient>(`${this.apiClient}`)
-  }
+    return this.http.get<IpClient>(`${this.apiClient}`);
+  };
 
   getWeatherByIp = (lat: number, lon: number): Observable<IpWeather> => {
-    return this.http.get<IpWeather>(`${this.apiWeatherUrl}lat=${lat}&lon=${lon}&appid=${this.apiWeatherKey}&units=metric`);
-  }
+    return this.http.get<IpWeather>(
+      `${this.apiWeatherUrl}lat=${lat}&lon=${lon}&appid=${this.apiWeatherKey}&units=metric`
+    );
+  };
 
   addMarkerToMap(data: IP, map: mapboxgl.Map) {
     this.marker = new Marker()
       .setLngLat([data.longitude, data.latitude])
-      .addTo(map)
+      .addTo(map);
   }
 
   displayMap(data: IP) {
@@ -49,9 +50,7 @@ export class IpDataService {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       zoom: 12,
-      center: [data.longitude, data.latitude]
+      center: [data.longitude, data.latitude],
     });
   }
-
-
 }
--- src/app/pages/ip/ip-info/ip-info.component.ts
+++ src/app/pages/ip/ip-info/ip-info.component.ts
@@ -1,22 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {IP} from "../ip-model/ip";
-import {IpDataService} from "../ip-data.service";
-import {ActivatedRoute} from "@angular/router";
-import {Title} from "@angular/platform-browser";
-import {IpWeather} from "../ip-model/ipweather";
+import { Component, Input, OnInit } from '@angular/core';
+import { IP } from '../ip-model/ip';
+import { IpDataService } from '../ip-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { IpWeather } from '../ip-model/ipweather';
 
 @Component({
   selector: 'app-ip-info',
   templateUrl: './ip-info.component.html',
-  styleUrls: ['./ip-info.component.scss']
+  styleUrls: ['./ip-info.component.scss'],
 })
 export class IpInfoComponent implements OnInit {
   @Input() data: IP | undefined;
   @Input() weatherData: IpWeather | undefined;
   ip: string = '';
 
-  constructor(private ipService: IpDataService, private route: ActivatedRoute, private title: Title) {
-  }
+  constructor(
+    private ipService: IpDataService,
+    private route: ActivatedRoute,
+    private title: Title
+  ) {}
 
   ngOnInit(): void {
     this.ip = this.route.snapshot.params['ip'];
@@ -25,13 +28,15 @@ export class IpInfoComponent implements OnInit {
       const map = this.ipService.displayMap(data);
       this.ipService.addMarkerToMap(data, map);
       this.title.setTitle(`${data.ip} (${data.country_name}) - GeoIP`);
-      this.ipService.getWeatherByIp(data.latitude, data.longitude).subscribe(weather => {
-        this.weatherData = weather;
-        this.weatherData.main.temp = Math.floor(weather.main.temp);
-        this.weatherData.main.feels_like = Math.floor(weather.main.feels_like);
-
-      })
-    })
+      this.ipService
+        .getWeatherByIp(data.latitude, data.longitude)
+        .subscribe((weather) => {
+          this.weatherData = weather;
+          this.weatherData.main.temp = Math.floor(weather.main.temp);
+          this.weatherData.main.feels_like = Math.floor(
+            weather.main.feels_like
+          );
+        });
+    });
   }
 }
-
