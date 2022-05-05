04ac188c54b99e669aafc0ba26f8c4c176ba5761
mb83pl <marcinbutora@192.168.1.104>
2022-05-05 13:59:58 +0200

    console.log

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,23 +1,20 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   ipForm = new FormGroup({
-    ip: new FormControl('', Validators.required)
-  })
+    ip: new FormControl('', Validators.required),
+  });
 
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) {}
 
   onCheck = () => {
-    this.router.navigate([`/ip/${this.ipForm.controls['ip'].value}`])
-    console.log(this.ipForm.controls['ip'].value)
-  }
+    this.router.navigate([`/ip/${this.ipForm.controls['ip'].value}`]);
+  };
 }
