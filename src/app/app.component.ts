c2d71f261e66b56d1784a55af6ff2034b04861f5
mb83pl <mbutora@gmail.com>
2022-06-10 09:41:34 +0200

    details again and correct spelling :-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {Title} from "@angular/platform-browser";
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   isCollapsed = true;
 
-  constructor(private title: Title) {
-  }
-
-  links = [
-    {path: '/', title: 'Home'},
-    {path: '/ip', title: 'Check Your IP'}
-  ]
+  constructor(private title: Title) {}
 
   ngOnInit() {
-    this.title.setTitle("GeoIP - localize your IP address on Map!")
+    this.title.setTitle('GeoIP - localize your IP address on Map!');
   }
 }
