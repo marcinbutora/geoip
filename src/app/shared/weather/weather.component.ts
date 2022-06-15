0fde88cb9d01274f8a7b6f0f916c48df2a7c1b78
mb83pl <mbutora@gmail.com>
2022-06-15 11:33:15 +0200

    some new weather info

--- src/app/shared/weather/weather.component.ts
+++ src/app/shared/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IpWeather } from 'src/app/pages/ip/ip-model/ipweather';
 
 @Component({
