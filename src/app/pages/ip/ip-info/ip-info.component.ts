63a6c5778ad16e80749a538c19b8abd2094950a3
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2022-12-01 10:10:43 +0100

    refactor

--- src/app/pages/ip/ip-info/ip-info.component.ts
+++ src/app/pages/ip/ip-info/ip-info.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IP } from '../ip-model/ip';
-import { IpDataService } from '../ip-data.service';
-import { ActivatedRoute } from '@angular/router';
-import { Meta, Title } from '@angular/platform-browser';
-import { IpWeather } from '../ip-model/ipweather';
-import { IpTimeZone } from '../ip-model/iptimezone';
-import { zip } from 'rxjs';
+import {Component, Input, OnInit} from '@angular/core';
+import {IP} from '../ip-model/ip';
+import {IpDataService} from '../ip-data.service';
+import {ActivatedRoute} from '@angular/router';
+import {Meta, Title} from '@angular/platform-browser';
+import {IpWeather} from '../ip-model/ipweather';
+import {IpTimeZone} from '../ip-model/iptimezone';
+import {zip} from 'rxjs';
 
 @Component({
   selector: 'app-ip-info',
@@ -28,19 +28,13 @@ export class IpInfoComponent implements OnInit {
     private route: ActivatedRoute,
     private title: Title,
     private metaService: Meta
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    this.ip = this.route.snapshot.params['ip'];
-    this.lat = this.route.snapshot.params['lat'];
-    this.lon = this.route.snapshot.params['lon'];
+    this.getParamsFromUrl();
     this.isLoading = true;
-
-    this.ipService.getDataByIP(this.ip).subscribe((val) => {
-      this.lat = val.latitude;
-      this.lon = val.longitude;
-    });
-
+    this.subscribeDataFromAPI();
     zip(
       this.ipService.getDataByIP(this.ip),
       this.ipService.getWeatherByLatAndLon(this.lat, this.lon),
@@ -56,31 +50,52 @@ export class IpInfoComponent implements OnInit {
           responseData.country_name
         )
       );
-      this.metaService.addTags([
-        {
-          name: 'description',
-          content: `${responseData.ip}, ${responseData.city}, ${responseData.country}, ${responseData.country_name}`,
-        },
-        { name: 'robots', content: 'index, follow' },
-        {
-          property: 'og:title',
-          content: this.setTitleForPage(
-            responseData.ip,
-            responseData.city,
-            responseData.country,
-            responseData.country_name
-          ),
-        },
-      ]);
-      const map = this.ipService.displayMap(this.data);
-      this.ipService.addMarkerToMap(this.data, map);
-      this.weatherData = responseWeather;
-      this.timezoneData = responseTimezone;
-      this.timezone = responseTimezone.formatted;
+      this.extractMetaTags(responseData);
+      this.responseWeatherAndTimezone(responseWeather, responseTimezone);
     });
   }
 
-  setTitleForPage = (
+  private responseWeatherAndTimezone(responseWeather: IpWeather, responseTimezone: IpTimeZone) {
+    const map = this.ipService.displayMap(this.data);
+    this.ipService.addMarkerToMap(this.data, map);
+    this.weatherData = responseWeather;
+    this.timezoneData = responseTimezone;
+    this.timezone = responseTimezone.formatted;
+  }
+
+  private extractMetaTags(responseData: IP) {
+    this.metaService.addTags([
+      {
+        name: 'description',
+        content: `${responseData.ip}, ${responseData.city}, ${responseData.country}, ${responseData.country_name}`,
+      },
+      {name: 'robots', content: 'index, follow'},
+      {
+        property: 'og:title',
+        content: this.setTitleForPage(
+          responseData.ip,
+          responseData.city,
+          responseData.country,
+          responseData.country_name
+        ),
+      },
+    ]);
+  }
+
+  private subscribeDataFromAPI() {
+    this.ipService.getDataByIP(this.ip).subscribe((val) => {
+      this.lat = val.latitude;
+      this.lon = val.longitude;
+    });
+  }
+
+  private getParamsFromUrl() {
+    this.ip = this.route.snapshot.params['ip'];
+    this.lat = this.route.snapshot.params['lat'];
+    this.lon = this.route.snapshot.params['lon'];
+  }
+
+  private setTitleForPage = (
     ip: string,
     city: string,
     country: string,
