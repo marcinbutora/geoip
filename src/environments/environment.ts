bf02c287238173b6adeab755baffef471bf4bd1e
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2023-01-22 12:29:16 +0100

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   styleOfMap: 'mapbox://styles/mapbox/streets-v11',
   defaultZoom: 12,
 };
