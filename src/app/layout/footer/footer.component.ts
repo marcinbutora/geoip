cd5776978909e5bdb597a46256613363dfb43258
Marcin Butora <mbutora@gmail.com>
2026-06-03 21:35:59 +0200

    fix: remove footer gsap animation causing gap

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,16 +1,11 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { gsap } from 'gsap';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements AfterViewInit {
+export class FooterComponent {
   authorMail = 'mbutora@gmail.com';
   actualYear = new Date().getFullYear();
-
-  ngAfterViewInit(): void {
-    gsap.from('footer', { y: 20, opacity: 0, duration: 0.5, delay: 0.8, ease: 'power2.out' });
-  }
 }
